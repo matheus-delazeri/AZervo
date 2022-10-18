16d9abf1c0997153a73ce18f53e618594f7aa0cf
matheus <matheus@getcommerce.com.br>
2022-10-18 17:27:04 -0300

    v1.3.6 - Ajustes visuais na exibição de mensagens

--- skin/js/script.js
+++ skin/js/script.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function () {
-    jQuery(".msg-container").slideDown(100, function() {
+    jQuery(".msg-container").slideDown(150, function() {
         $(this).css('display', 'flex');
     })
     setTimeout(() => {
-        jQuery(".msg-container").slideUp(100)
-    }, 5000)
+        jQuery(".msg-container").slideUp(150)
+    }, 4000)
 })
