52f90fb5868c58856311f61f7faddbc9f336b03e
Matheus Delazeri <matheusdelazeris@gmail.com>
2022-10-25 16:03:21 -0300

    v1.4.1 - Ajuste nos parâmetros do CURL

--- app/Model/Api.php
+++ app/Model/Api.php
@@ -49,13 +49,8 @@ class Api
         curl_setopt_array($curl, array(
             CURLOPT_URL => $url,
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_SSL_VERIFYHOST => false,
             CURLOPT_SSL_VERIFYPEER => false,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "GET"
         ));
 
